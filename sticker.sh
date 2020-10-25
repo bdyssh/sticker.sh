44671cb68ea605a776f8d21ec9d12a0458a4cfec
bdyssh <73402976+bdyssh@users.noreply.github.com>
2020-10-25 15:35:01 +0300

    Add files via upload

--- sticker.sh
+++ sticker.sh
@@ -8,10 +8,11 @@
 #	PT-1230PC	180 dpi		64 px
 #	PT-2430PC	180 dpi		128 px
 #	PT-9800PCN	360 dpi		384 px
-#	PT-P950NW	360 dpi		454 px
+#	PT-P950NW	360 dpi		454 px (virt. 560 px?)
 
 # Name, take it from 'system-config-printer' or so:
-printer="PT-9800PCN"
+printer="PT-P950"
+#printer="PT-9800PCN"
 #printer="PT-2430PC"
 
 # https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
@@ -127,6 +128,7 @@ fi
 # The nearest prject to talk with USB printer is like libinklevel-0.9.3.tar.gz.
 # but still, request to receive status is unknown and can be known
 # only from rev.eng. of USB flow from Windows(r) driver.
+# "Appendix A: USB Specifications" at "Raster Command Reference" says nothing new.
 # Also may be USB-ETH wrapper can be born in future somewhere.
 # So currently we will ask user for installed tape width for USB printer.
 
@@ -167,12 +169,12 @@ if (($use_snmp>0)); then
 		echo " Type  $printer_type"
 	fi
 
+# 1.3.6.1.2.1.43.16.5.1.2.1.1
 # also status as integer: 1.3.6.1.2.1.43.18.1.1.6.1.1 = INTEGER: 10001 (good), or others.
 	printer_status=$(snmpget -Oqv -v 1 -c public $printer_ip 1.3.6.1.2.1.43.16.5.1.2.1.1)
 	if (($verbose>0)); then
 		echo " Status  $printer_status"
 	fi
-
 # wildcards / partial match - looks like work. 23.10.2020
 # https://stackoverflow.com/questions/2237080/how-to-compare-strings-in-bash
 	if [[ "$printer_status" == *READY* ]]; then  
@@ -228,8 +230,12 @@ case "$printer_type" in
 		printhead_dots=384
 		need_extra_header=1
 		;;
-	*950NW*) # wildcards / partial match / substring match
-		printhead_dots=454
+	*P950*) # wildcards / partial match / substring match
+# Defined by specs:
+#		printhead_dots=454
+# But, with 454 not work (shifted). Rev.eng. of cups printouts also show strange "G" (0x47) codes.
+# "Raster Command Reference" says: total number of pins (560)! What is true?
+		printhead_dots=560 # now almost centered...
 		need_extra_header=1
 		;;
 	*)
@@ -247,8 +253,8 @@ case "$installed_tape" in
 # 36mm should be before 6mm
 *36mm*) # wildcards / partial match / substring match
 	tape_width="36"
-	tape_pixels=512
-#	tape_pixels=42
+	tape_pixels=454 # from table
+#	tape_pixels=42 debug
 	;;
 *3.5mm*) # wildcards / partial match / substring match
 	tape_width="3.5"
@@ -256,23 +262,32 @@ case "$installed_tape" in
 	;;
 *6mm*) # wildcards / partial match / substring match
 	tape_width="6"
-	tape_pixels=84
+	tape_pixels=64
 	;;
 *9mm*) # wildcards / partial match / substring match
 	tape_width="9"
-	tape_pixels=128
+	tape_pixels=106 # from table
 	;;
 *12mm*) # wildcards / partial match / substring match
 	tape_width="12"
-	tape_pixels=168
+	tape_pixels=150 # from table
+#	tape_pixels=152 # from table and corrected to 8*INT
+#	tape_pixels=160 # tested, do not change.
 	;;
 *18mm*) # wildcards / partial match / substring match
 	tape_width="18"
-	tape_pixels=256
+	tape_pixels=234 # from table
+#	tape_pixels=232 # from table and corrected to 8*INT
+#	tape_pixels=248 # tested, do not change.
 	;;
 *24mm*) # wildcards / partial match / substring match
 	tape_width="24"
-	tape_pixels=340
+#	tape_pixels=168 debug
+#	tape_pixels=336 # tested with 9800PCN, do not change.
+# For P950NW, there is auto internal limit for all widths, here 
+# it is 320 px. Extra pixels are eaten and thrown away. It is bad, but
+# saves head when possible tape skews. So better to use 320 (as from table).
+	tape_pixels=320
 	;;
 *)
 	echo "Unknown installed tape description $installed_tape. Please examine it via snmp or printer specs, and fix me, or try to use other tape."
@@ -292,33 +307,23 @@ if (($tape_pixels>$printhead_dots)); then
 fi
 
 margin=$(( ($printhead_dots - $tape_pixels)/2 ))
-printhead_bytes=$(( $printhead_dots/8 ))
-if (($printhead_bytes>255)); then
-	echo "ERROR. Image too large. Sorry."
-	exit 1
-fi
-
-#tape_bytes=$(( ($tape_pixels - 1) / 8 + 1 ))
+printhead_bytes=$(( ($printhead_dots + 7) / 8 ))
 
 if (($verbose>0)); then
-#	echo " Tape width (parsed) $tape_width ($tape_pixels px), head dots $printhead_dots (bytes $printhead_bytes), margin $margin, tape bytes $tape_bytes."
 	echo " Tape width (parsed) $tape_width ($tape_pixels px), head dots $printhead_dots (bytes $printhead_bytes), margin $margin."
 fi
 
-
-# printer="PT-2430PC"; if ((`lpstat -p -d | grep "$printer" | wc -c`)) > 0; then echo $printer not found.; break; fi; w=16; echo "" > pt.prn; for((i=0;i<255;i++)); do echo -n -e \\x00 >> pt.prn; done; convert "$1" -flop -rotate 90 -background white -gravity center -extent $((w*8))x -monochrome -colors 2 -depth 1 -negate r:img.raw; for((i=0;i< (`stat --format="%s" img.raw`/$w); i++)); do echo -n -e "G"\\x10\\x00 >> pt.prn; dd if=img.raw of=pt.prn bs=$w count=1 skip=$i oflag=append conv=notrunc > /dev/null 2>&1; done; echo -n -e \\x1a >> pt.prn; lpr -C "$1" -P"$printer" -o raw pt.prn
-
-
-# Main job
+# Main job: prepare print file with raw binary data.
 
 echo "" > pt.prn
 
+# resets any prev. transfer (include incomplete) if any; min. 200 zeroes.
 for((i=0; i<255; i++)); do echo -n -e \\x00 >> pt.prn; done;
 
 if (($need_extra_header>0)); then
 # clear buffer and transferred lines q'ty.
 	echo -n -e \\x1b\\x40 >> pt.prn		
-# unknown, but required for 9800PCN at least.
+# unknown, but required for 9800PCN at least. And for P950NW also.
 	echo -n -e \\x1b\\x69\\x61\\x01 >> pt.prn 
 # Its work,  set (zero) margins
 #	echo -n -e \\x1b\\x69\\x64\\x00\\x00 >> pt.prn  
@@ -329,6 +334,7 @@ fi
 convert -quiet "$img" -resize "$enlarge"00% -flop -rotate 90 temp1.png
 
 img_w=`identify -ping -format '%w' temp1.png`
+# min img_h at 360 dpi = 57 px, max = 14173 px, check it later. TODO
 img_h=`identify -ping -format '%h' temp1.png`
 pages=$(( ($img_w-1) / $tape_pixels + 1 ))
 if (($verbose>0)); then
@@ -352,10 +358,27 @@ convert -quiet temp1.png -background white -gravity center -extent "$total_width
 
 for((copy=0; copy<$copies; copy++)); do 
   for((page=0; page<$pages; page++)); do 
+	first_middle_last_page=1 # middle
+	if ((copy == 0)); then
+		if ((page == 0)); then
+			first_middle_last_page=0 # first
+		fi
+	fi
+	if ((copy == $copies-1)); then
+		if ((page == $pages-1)); then
+			first_middle_last_page=2 # last or single page
+		fi
+	fi
+  
 	if (($need_extra_header>0)); then
-# clear buffer and transferred lines q'ty.
+# Clear buffer and transferred lines q'ty. It is important for every 'page'.
 		echo -n -e \\x1b\\x40 >> pt.prn		
-# will transfer bitmap after it.
+# This ESC seq. required for PT-P9100/900W/950NW (and maybe PT-P910BT); 
+#  are others like 9800PCN can live with it ?! TODO Added: Yes, 9800PCN not fear it.
+		h_lsbyte=$(( $img_h % 256 ))
+		h_msbyte=$(( $img_h / 256 ))
+		echo -n -e \\x1b\\x69\\x7a\\x80\\x00\\x00\\x00\\x$(printf %x "$h_lsbyte")\\x$(printf %x "$h_msbyte")\\x00\\x00\\x$(printf %x "$first_middle_last_page")\\x00 >> pt.prn 
+# Will transfer bitmap after it.
 		echo -n -e \\x4d\\x00 >> pt.prn
 		echo -n -e \\x1b\\x69\\x52\\x01 >> pt.prn
 	fi
@@ -373,12 +396,10 @@ for((copy=0; copy<$copies; copy++)); do
 		dd if=img.raw of=pt.prn bs=$printhead_bytes count=1 skip=$i oflag=append conv=notrunc > /dev/null 2>&1; 
 	done; 
 
-# This works for both USB and network printers, and maybe many others.
+# This works for both USB and network printers, and maybe some others.
 	eop_marker="\\x0c" # page feed (+ half cut if possible).
-	if ((copy == $copies-1)); then
-		if ((page == $pages-1)); then
-			eop_marker="\\x1a" # done print job (cut tape).
-		fi
+	if (($first_middle_last_page == 2)); then
+		eop_marker="\\x1a" # done print job (cut tape).
 	fi
 	echo -n -e "$eop_marker" >> pt.prn; 
   done; 
